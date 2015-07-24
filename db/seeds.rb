30d575a8cb72e37ea31179f5aa1ecec7028e4c5e
Nhu Nguyen <nhu313@gmail.com>
2015-07-24 13:17:59 -0400

    cleaned up seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,12 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Tag.delete_all
+["Main dish","Vegan","Dessert","Breakfast","Vegetarian",
+ "Side dish","Salad","Soup & Stew","Noodle","Pasta",
+ "Meat","Pork","Beef","Chicken","Tofu","Grill","Meal"].each do | name |
+    Tag.find_or_create_by(name: name)
+ end
 
-Tag.create(id: 1, name: "Main dish")
-Tag.create(id: 2, name: "Vegan")
-Tag.create(id: 3, name: "Dessert")
-Tag.create(id: 4, name: "Breakfast")
-Tag.create(id: 5, name: "Vegetarian")
-Tag.create(id: 6, name: "Side dish")
-Tag.create(id: 7, name: "Salad")
-Tag.create(id: 8, name: "Soup & Stew")
-Tag.create(id: 9, name: "Noodle")
-Tag.create(id: 10, name: "Pasta")
-Tag.create(id: 11, name: "Meat")
-Tag.create(id: 12, name: "Pork")
-Tag.create(id: 13, name: "Beef")
-Tag.create(id: 14, name: "Chicken")
-Tag.create(id: 15, name: "Tofu")
-Tag.create(id: 16, name: "Grill")
-Tag.create(id: 17, name: "Meal")
-
+require 'json'
+recipes = JSON.parse(File.read("#{Rails.root}/db/pwmf_vegan_recipes.json"))
+recipes.each {|r| Recipe.create(r.merge(user_id: 1))}
