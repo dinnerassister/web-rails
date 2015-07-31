ac0dbfec9a7bbe7ae8c576edbfb6c113441b79bb
Hana Park <phablue@gmail.com>
2015-07-30 21:55:25 -0700

    detail fields data change when ingredient name change

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,21 +1,25 @@
 class Ingredient < ActiveRecord::Base
   belongs_to :recipe
-  after_create :update_detail_field
+  after_create :update_detail_fields
+  after_update :update_detail_fields, if: :user_input_change? 
 
   private
-  def update_detail_field
-    self.update(detail_field)
+  def update_detail_fields
+    (3..6).each do |n|
+      self[attribute_keys[n]] = parse_user_input[n - 3]
+    end
+    self.save
   end
 
-  def detail_field
-    { item: parse_user_input[0],
-      quantity: parse_user_input[1],
-      unit: parse_user_input[2],
-      description: parse_user_input[3]
-    }
+  def attribute_keys
+    self.attribute_names
   end
 
   def parse_user_input
     self.name.split
+  end
+
+  def user_input_change?
+    self.changed == ["name"]
   end
 end
